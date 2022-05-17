2752ff30985f507e4d51d85fbfd24bf59cac0a42
unknown <ritammoharana32@gmail.com>
2022-05-17 18:21:24 +0530

    test commit1

--- demo_project/demo_project/settings.py
+++ demo_project/demo_project/settings.py
@@ -24,7 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-8$w!m0lred%@8wum!*bl$-t%@v!1wj60704ydxa75s@)2tgvsu'
+#SECRET_KEY = 'django-insecure-8$w!m0lred%@8wum!*bl$-t%@v!1wj60704ydxa75s@)2tgvsu'
+SECRET_KEY = '${{ secrets.DJANGO_SECRET_KEY }}'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
